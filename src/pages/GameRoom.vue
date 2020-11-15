181814e8aac6e644f0f01a0919c1eee36cbcc30e
Joao Nascimento <joao.nascimento@sensedia.com>
2020-11-15 15:16:38 -0300

    Fix scroll

--- src/pages/GameRoom.vue
+++ src/pages/GameRoom.vue
@@ -1,10 +1,11 @@
-<template>
-  <main>
+<template class='overscroll'>
+  <main class='overscroll'>
     <div
       v-if="!showLetter"
       v-bind:class="[
-        globalContrastVariable.applyContrast ? 'gameRoom-contrast' : 'gameRoom',
+        globalContrastVariable.applyContrast ? 'gameRoom-contrast overscroll' : 'gameRoom overscroll',
       ]"
+      
     >
       <h1
         v-bind:class="[
@@ -288,7 +289,6 @@ html {
   left: 0;
   right: 0;
   box-shadow: 0 0 10vh dimgrey;
-  overflow: hidden;
   background-color: #ffffff;
   background-image: url("data:image/svg+xml,%3Csvg width='64' height='64' viewBox='0 0 64 64' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M8 16c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zm33.414-6l5.95-5.95L45.95.636 40 6.586 34.05.636 32.636 2.05 38.586 8l-5.95 5.95 1.414 1.414L40 9.414l5.95 5.95 1.414-1.414L41.414 8zM40 48c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zM9.414 40l5.95-5.95-1.414-1.414L8 38.586l-5.95-5.95L.636 34.05 6.586 40l-5.95 5.95 1.414 1.414L8 41.414l5.95 5.95 1.414-1.414L9.414 40z' fill='%23c7c7c7' fill-opacity='0.19' fill-rule='evenodd'/%3E%3C/svg%3E");
 }
@@ -416,5 +416,10 @@ html {
 
 .back:hover {
   background: rgb(255, 241, 50);
+}
+
+.overscroll {
+  max-height: 100%; 
+  overflow-y: auto !important;
 }
 </style>
